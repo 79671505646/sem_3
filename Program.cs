7c26b1e90f1f81549e40cd62744b706fd2085234
79671505646 <116814182+79671505646@users.noreply.github.com>
2022-11-20 20:31:06 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,76 @@
+﻿/* Задача 19
+ Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+
+Console.WriteLine("Введите пятизначное число число: ");
+string number = Console.ReadLine();
+int len = number.Length;
+
+if (len == 5)
+{
+    if (number[0] == number[4] && number[1] == number[3])
+    {
+        Console.WriteLine($"{number} - Палиндром");
+    }
+    else
+    {
+        Console.WriteLine($"{number} - НЕ палиндром");
+    }
+}
+else
+{
+    Console.WriteLine($"ОШИБКА: {number} - не является пятизначным");
+}
+*/
+
+
+/*Задача 21
+Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+A (3,6,8); B (2,1,-7), -> 15.84
+A (7,-5, 0); B (1,-1,9) -> 11.53
+*/
+
+/*
+int numX1 = ReadInt("Введите координату X первой точки: ");
+int numY1 = ReadInt("Введите координату Y первой точки: ");
+int numZ1 = ReadInt("Введите координату Z первой точки: ");
+
+int numX2 = ReadInt("Введите координату X второй точки: ");
+int numY2 = ReadInt("Введите координату Y второй точки: ");
+int numZ2 = ReadInt("Введите координату Z второй точки: ");
+
+int A = numX2 - numX1;
+int B = numY2 - numY1;
+int C = numZ1 - numZ2;
+
+double length = Math.Sqrt(A * A + B * B + C * C);
+Console.WriteLine($"Длинна отрезка {length}");
+
+
+// Функция ввода сообщения
+int ReadInt(string message)
+{
+ Console.Write(message);
+ return Convert.ToInt32(Console.ReadLine());
+}
+*/
+
+/*
+Задача 23
+Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+3 -> 1, 8, 27
+5 -> 1, 8, 27, 64, 125
+
+
+int number = ReadInt("Введите число N: ");
+
+for (int i = 1; i <= number; i++)
+{ 
+    Console.Write($"{i*i*i} ");
+}
+
+// Функция ввода сообщения
+int ReadInt(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+} */
